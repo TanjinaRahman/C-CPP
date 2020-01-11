04a86fc5050fa5a30021b2caa705c2c097b4d305
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2020-01-12 00:45:40 +0600

    Output in csv file

--- assignment.c
+++ assignment.c
@@ -4,21 +4,28 @@ int i,j,Vt=1, c=30, Vgs=2, Ids,Vds,diff;
 
 int main(){
 
+FILE * fp = fopen ("Output.csv" , " w+ ");
+fprintf (fp, "%s","Vds,Ids\n");
 for(j=0;j<4;j++){
-  //printf("j=%d\n",j);
+
         for( Vds=0;Vds<6;Vds++){
+
             if(Vds<(Vgs-Vt)){
                 Ids=c*(((Vgs-Vt)*Vds)-(Vds*Vds)/2);
             }
+
             else if(Vds>=(Vgs-Vt))
             {
                 Ids=c*((Vgs-Vt)*(Vgs-Vt))/2;
                 //printf("??");
 
             }
-          printf("%d %d\n",Vds,Ids);
+
+          //printf("%d %d\n",Vds,Ids);
+          fprintf (fp, "%d,%d\n",Vds,Ids);
 
         }
+
         Vgs++;
 
 
