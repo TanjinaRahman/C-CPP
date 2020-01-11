6ea691104fe5124a72fe845e9a19665f13f238fa
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2020-01-12 00:14:54 +0600

    updated

--- assignment.c
+++ assignment.c
@@ -1,25 +1,27 @@
 #include<stdio.h>
 
-int i,Vt=1, c=30, Vgs, Ids[6], Vds[]={0,1,2,3,4,5};
+int i,j,Vt=1, c=30, Vgs=2, Ids,Vds,diff;
 
 int main(){
-Vgs=2;
-while(Vgs<6){
-        Ids[0]=0;
-for( i=1;i<=5;i++){
-    if(Vds[i]<(Vgs-Vt)){
-        Ids[i]=c*(Vgs-Vt)*Vds[i];
-    }
-    else
-    {
-        Ids[i]=1/2*c*((Vgs-Vt)*(Vgs-Vt));
 
-    }
-//  printf("%d",Ids[i]);
-}
+for(j=0;j<4;j++){
+  //printf("j=%d\n",j);
+        for( Vds=0;Vds<6;Vds++){
+            if(Vds<(Vgs-Vt)){
+                Ids=c*(((Vgs-Vt)*Vds)-(Vds*Vds)/2);
+            }
+            else if(Vds>=(Vgs-Vt))
+            {
+                Ids=c*((Vgs-Vt)*(Vgs-Vt))/2;
+                //printf("??");
 
-  printf("\n%d\n\n",Vgs);
-Vgs++;
+            }
+          printf("%d %d\n",Vds,Ids);
+
+        }
+        Vgs++;
+
+
 }
 
 return 0;
