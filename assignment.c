30c8de41f0fbc32f0faff884ecf14e65d9a4fe72
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2020-01-13 09:11:18 +0600

    Final

--- assignment.c
+++ assignment.c
@@ -1,9 +1,17 @@
 #include<stdio.h>
 
-int j, Vt=1, c=30, Vgs=2, Ids, Vds;
+int i,j, Vt=1, c, Vgs=2, Ids, Vds;
 
 int main(){
 
+printf("Press 1 for nMOS and press any key for pMOS\n");
+scanf("%d",&i);
+if (i==1){
+        c=30;
+}
+else{
+    c=15;
+}
 FILE * fp = fopen ("Output.csv" , " w+ ");
 fprintf (fp, "%s","Vds,Ids\n");
 for(j=0;j<4;j++){
